f2d33acb7fd486cf54c8538ced0d0707f95c6be4
aonkd <a.j.lambert23@gmail.com>
2023-10-01 19:46:57 -0400

    completes project part 2

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -5,9 +5,22 @@ var max_speed = 600.0
 var speed_multiplier = 1.0
 var accelerate = false
 
+var time_highlight = 0.4
+var time_highlight_size = 0.3
+
+var wobble_period = 0.0
+var wobble_amplitude = 0.0
+var wobble_max = 5
+var wobble_direction = Vector2.ZERO
+var decay_wobble = 0.15
+
 var released = true
 
 var initial_velocity = Vector2.ZERO
+
+var tween
+
+var distort_effect = 0.03
 
 func _ready():
 	contact_monitor = true
@@ -21,10 +34,19 @@ func _ready():
 	
 
 func _on_Ball_body_entered(body):
-	$highlight.modulate.a = 1
+	$Images/Highlight.modulate.a = 1
 	if body.has_method("hit"):
 		body.hit(self)
-		accelerate = true	
+		accelerate = true
+	if tween:
+		tween.kill()
+	tween = create_tween().set_parallel(true)
+	$Images/Highlight.modulate.a = 1.0
+	tween.tween_property($Images/Highlight, "modulate:a", 0, time_highlight).set_trans(Tween.TRANS_LINEAR)
+	$Images/Highlight.scale = Vector2(2.0,2.0)
+	tween.tween_property($Images/Highlight, "scale", Vector2(1.0,1.0),time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	wobble_direction = linear_velocity.orthogonal().normalized()
+	wobble_amplitude = wobble_max
 
 func _input(event):
 	if not released and event.is_action_pressed("release"):
@@ -32,8 +54,8 @@ func _input(event):
 		released = true
 
 func _integrate_forces(state):
-	if $highlight.modulate.a > 0:
-		$highlight.modulate.a -= 0.05
+	if $Images/Highlight.modulate.a > 0:
+		$Images/Highlight.modulate.a -= 0.05
 	if not released:
 		var paddle = get_node_or_null("/root/Game/Paddle_Container/Paddle")
 		if paddle != null:
@@ -52,11 +74,23 @@ func _integrate_forces(state):
 		state.linear_velocity = state.linear_velocity.normalized() * max_speed * speed_multiplier
 
 func change_size(s):
-	$ColorRect.scale = s
+	$Images/ColorRect.scale = s
 	$CollisionShape2D.scale = s
 
 func change_speed(s):
 	speed_multiplier = s
+
+func wobble():
+	wobble_period += 1
+	if wobble_amplitude > 0:
+		var pos = wobble_direction * wobble_amplitude * sin(wobble_period)
+		$Images.position = pos
+		wobble_amplitude -= decay_wobble
+
+func distort():
+	var direction = Vector2(1 + linear_velocity.length() * distort_effect, 1 - linear_velocity.length() * distort_effect)
+	$Images.rotation = linear_velocity.angle()
+	$Images.scale = direction
 
 func die():
 	queue_free()
